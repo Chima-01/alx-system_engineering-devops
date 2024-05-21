9eca7596555c25579b75a51b59c643390012cec2
chima-01 <divinechima76@gmailo.com>
2024-05-21 04:07:26 -0700

    fixed bug in task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # puppet manifest, install flask from pip3
 
-package { 'pip':
-  ensure  => 'installed',
-}
-
-exec { 'flask':
-  command => '/usr/bin/pip install flask',
-  require => Package['pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
