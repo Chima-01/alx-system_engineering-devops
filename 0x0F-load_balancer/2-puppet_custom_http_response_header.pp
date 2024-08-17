bf2936f00938d00f98b565c81b99b1a176c8f369
chima-01 <divinechima76@gmail.com>
2024-08-16 18:10:50 -0700

    created a custom header using puppet manifest

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# add custom header to nginx server
+
+exec { 'update server':
+  provider => shell,
+  command  => 'sudo apt-get -y update'
+  before   => Exec['install_nginx'],
+}
+
+
+exec { 'install_nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['custom_header'],
+}
+
+
+exec { 'custom_header':
+  provider    => shell,
+  environment => ["Hostname=${::hostname"}],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart server'],
+}
+
+exec { 'restart_server':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
