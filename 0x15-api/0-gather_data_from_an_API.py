bf4896fee5ca5115b1565e6e59575887a0dc5a7d
chima-01 <divinechima76@gmail.com>
2024-09-02 12:20:24 -0700

    added documentation

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,17 +6,19 @@
 import requests
 from sys import argv
 
-user_id = argv[1]
-url = 'https://jsonplaceholder.typicode.com'
-user = f'{url}/users/{user_id}'
-todos = f'{url}/todos?userId={user_id}'
-data = requests.get(user).json()
-user_todos = requests.get(todos).json()
-username = data.get('name')
-task_done = [task['title'] for task in user_todos if task['completed']]
 
-print(f'Employee {username} is done with tasks {len(task_done)}/\
-{len(user_todos)}')
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = f'{url}/users/{user_id}'
+    todos = f'{url}/todos?userId={user_id}'
+    data = requests.get(user).json()
+    user_todos = requests.get(todos).json()
+    username = data.get('name')
+    task_done = [task['title'] for task in user_todos if task['completed']]
 
-for task in task_done:
-    print(f'\t {task}')
+    print(f'Employee {username} is done with tasks ({len(task_done)}/\
+{len(user_todos)})')
+
+    for task in task_done:
+        print(f'\t {task}')
