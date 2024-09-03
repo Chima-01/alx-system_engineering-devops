520e98a25e6e441876fd4dccf92639ade891bc51
chima-01 <divinechima76@gmail.com>
2024-09-03 07:56:06 -0700

    append output to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    extend your Python script to export data in the CSV format
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = f'{url}/users/{user_id}'
+    todos = f'{url}/todos?userId={user_id}'
+    username = requests.get(user).json().get('name')
+    tasks = requests.get(todos).json()
+
+    with open((user_id + '.csv'), 'w') as f:
+        output = csv.writer(f, delimiter=",", quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+
+        for task in tasks:
+            output.writerow([task['userId'], username, task['completed'],
+                            task['title']])
+
+    f.close()
